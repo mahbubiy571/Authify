21d439515fc02d5d65518deec4d5fe24c6ac8871
avazbek <avazbek@gmail.com>
2025-09-03 17:43:44 +0500

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
-import Register, { action } from "./pages/Register";
+import Register from "./pages/Register";
 import Home from "./pages/Home";
 import MainLayout from "./layouts/MainLayout";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 import { useDispatch, useSelector } from "react-redux";
-import { action as RegisterAction } from "./pages/Register";
-import { action as LoginAction } from "./pages/Login";
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase/config";
@@ -19,32 +13,6 @@ import { isAuthReady, login } from "./app/features/userSlice";
 function App() {
   const dispatch = useDispatch();
   const { user, authReady } = useSelector((store) => store.user);
-  const routes = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <ProtectedRoutes user={user}>
-          <MainLayout />
-        </ProtectedRoutes>
-      ),
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-      ],
-    },
-    {
-      path: "/login",
-      element: user ? <Navigate to="/" /> : <Login />,
-      action: LoginAction,
-    },
-    {
-      path: "/register",
-      element: user ? <Navigate to="/" /> : <Register />,
-      action: RegisterAction,
-    },
-  ]);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -54,7 +22,36 @@ function App() {
       dispatch(isAuthReady(true));
     });
   }, []);
-  return <>{authReady && <RouterProvider router={routes} />}</>;
+
+  return (
+    <>
+      {authReady && (
+        <HashRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoutes user={user}>
+                  <MainLayout />
+                </ProtectedRoutes>
+              }
+            >
+              <Route index element={<Home />} />
+            </Route>
+
+            <Route
+              path="/login"
+              element={user ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+              path="/register"
+              element={user ? <Navigate to="/" /> : <Register />}
+            />
+          </Routes>
+        </HashRouter>
+      )}
+    </>
+  );
 }
 
 export default App;
