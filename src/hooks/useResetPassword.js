3fbd7be0c966e2c16915f099976743291b778706
avazbek <avazbek@gmail.com>
2025-09-03 13:34:29 +0500

    ...

--- src/hooks/useResetPassword.js
+++ src/hooks/useResetPassword.js
@@ -5,7 +5,9 @@ import { sendPasswordResetEmail } from "firebase/auth";
 export const useResetPassword = () => {
   const resetPassword = async (email) => {
     try {
-      await sendPasswordResetEmail(auth, email);
+      await sendPasswordResetEmail(auth, email, {
+        url: "http://localhost:5173/",
+      });
       alert("Check your email");
     } catch (error) {
       const errorMessage = error.message;
